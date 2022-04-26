61ce9f9628be67b09a98541e4954d0a067a8616a
Nastya Grigoreva <grinastich@gmail.com>
2022-04-26 21:57:55 +0500

    module2-task1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import {createTripSortTemplate} from '.view/trip-sort-view.js';
-import {createTripFiltersTemplate} from '.view/trip-filter-view.js';
-import {createSiteMenuTemlete} from './view/site-menu-view.js';
-import {createAddEventItemTemplate} from './view/add-new-event-form';
+import {renderTemplate, RenderPosition} from './render.js';
+import {createTripTabsTemplate} from './view/site-menu-view.js';
+import {createTripFiltersTemplate} from './view/trip-filter-view.js';
+import {createTripSortTemplate} from './view/trip-sort-view.js';
+import {createAddEventItemTemplate} from './view/add-new-event-form.js';
 import {createEditedEventItemTemplate} from './view/trip-events-edit-form';
-import {createTripEventItemTemplate} from './view/trip-events-item.js';
+import {createTripEventsItemTemplate} from './view/trip-events-item-view.js';
 import {createEventsListTemplate} from './view/events-list-view.js';
 import {generateTripEvent} from './mock/events';
-import {renderTemplate, RenderPosition} from './render.js';
 
 const Trip_Count = 20;
 
 const tripEvents = Array.from({length: Trip_Count}, generateTripEvent);
+
 
 const TripControlsNavigationElement = document.querySelector('.trip-controls__navigation');
 const TripControlsFiltersElement = document.querySelector('.trip-controls__filters');
@@ -20,14 +21,11 @@ renderTemplate(TripEventsElement, createEventsListTemplate(), RenderPosition.BEF
 
 const TripEventsListElement = TripEventsElement.querySelector('.trip-events__list');
 
-renderTemplate(TripControlsNavigationElement, createSiteMenuTemlete(), RenderPosition.BEFOREEND);
+renderTemplate(TripControlsNavigationElement, createTripTabsTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(TripControlsFiltersElement, createTripFiltersTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(TripEventsElement, createTripSortTemplate(), RenderPosition.AFTERBEGIN);
-renderTemplate(TripEventsListElement, createTripEventEditTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(TripEventsListElement, createTripEventItemTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(TripEventsListElement, createEditedEventItemTemplate(tripEvents[1]), RenderPosition.AFTERBEGIN);
 renderTemplate(TripEventsListElement, createAddEventItemTemplate(tripEvents[0]), RenderPosition.AFTERBEGIN);
-for (let i = 2; i < TRIP_EVENTS_COUNT; i++) {
-
-  renderTemplate(TripEventsListElement, createTripEventItemTemplate(tripEvents[i]), RenderPosition.BEFOREEND);
+for (let i = 2; i < Trip_Count; i++) {
+  renderTemplate(TripEventsListElement, createTripEventsItemTemplate(tripEvents[i]), RenderPosition.BEFOREEND);
 }
