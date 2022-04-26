1e31d1b89cad598342ab58036bffbea6417a4a72
Nastya Grigoreva <grinastich@gmail.com>
2022-04-26 22:07:42 +0500

    second commit

--- src/view/trip-events-item-view.js
+++ src/view/trip-events-item-view.js
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 
-export const createTripEventItemTemplate = (tripEvent) => {
+export const createTripEventsItemTemplate = (tripEvent) => {
 
   const {eventType, location, price, startDate, endDate, duration, offers, isFavorite} = tripEvent;
   const startDay = dayjs(startDate).format('MMM D');
@@ -67,9 +67,7 @@ export const createTripEventItemTemplate = (tripEvent) => {
                   <span class="visually-hidden">Add to favorite</span>
                   <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
                     <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
-                  </svg>
-                </button>
-                <button class="event__rollup-btn" type="button">
+	@@ -36,5 +73,5 @@ export const createTripEventsItemTemplate = () => (
                   <span class="visually-hidden">Open event</span>
                 </button>
               </div>
